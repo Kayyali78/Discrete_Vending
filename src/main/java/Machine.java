a8920d0a137ac37e3c7824581e087f093ba5946d
Scarlett Weeks <sweeks@oswego.edu>
2023-12-14 11:39:20 -0500

    Final simulation settings

--- src/main/java/Machine.java
+++ src/main/java/Machine.java
@@ -1,67 +1,102 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Machine {
-    State state;
-    double continuousTime = 0.0;
+    State state = new State();
+    ;
+    double continuousTime = 0.0, elapsedTime = 0.0;
     int[] outCounts = new int[3];
     InputPair inputPair;
-
+    int coffeeCost = 100;
 
     /*
     This program is a simulation of a coffee machine. It takes in a single input at a time consisting of nickels, dimes, and quarters.
     It outputs a coffee for every 100 cents and gives change in the form of quarters, dimes, and nickels.
      */
-    public void main(String[] args){
-        state = new State(0,0,0,0);
-        startSimulation();
-        //state is declared at 0,0,0,0
-        //lambda before delta
-        //inputPair is taken in, time elapsed is used to change the time value
+
+    public static void main(String[] args) {
+        Machine machine = new Machine();
+        machine.startSimulation();
     }
 
-    private void startSimulation(){
-        if (inputPair.continuousTime < continuousTime){
-            deltaExternal(inputPair);
-        } else if (inputPair.continuousTime == continuousTime){
-            deltaConfluent(inputPair);
-        } else {
-            deltaInternal();
+    private void startSimulation() {
+        boolean loop = true;
+        while (loop) {
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Enter an input pair (format: continuousTime,discreteTime,inputChar): ");
+
+            // Read and parse input
+            String input = scanner.next().trim();
+            String[] inputParts = input.split(",");
+
+            if (inputParts.length != 3) {
+                System.out.println("Invalid input format. Please provide input in the correct format.");
+                return;
+            }
+
+            try {
+                double continuous = Double.parseDouble(inputParts[0]);
+                int discrete = Integer.parseInt(inputParts[1]);
+                char inputChar = inputParts[2].charAt(0);
+
+                inputPair = new InputPair(continuous, discrete, inputChar);
+                if (continuousTime == 0.0) {
+                    continuousTime = inputPair.getContinuousTime();
+                    deltaExternal(inputPair);
+                } else if (inputPair.getContinuousTime() < continuousTime) {
+                    System.out.println("InputPair continuous time is less than current continuous time.");
+                    deltaExternal(inputPair);
+                } else if (inputPair.getContinuousTime() == continuousTime) {
+                    System.out.println("InputPair continuous time is equal to current continuous time.");
+                    deltaConfluent(inputPair);
+                } else if ((inputPair.getContinuousTime() == 0) && (inputPair.getDiscreteTime() == 0) && (inputPair.getInput() == 'x')) {
+                    System.out.println("Ending simulation.");
+                    change();
+                    loop = false;
+                } else {
+                    System.out.println("InputPair continuous time is greater than current continuous time.");
+                    deltaInternal();
+                }
+            } catch (NumberFormatException | StringIndexOutOfBoundsException e) {
+                System.out.println("Invalid input values. Please provide valid numeric values and a character.");
+            }
         }
     }
 
-    private double timeAdvance(){
-        if (state.v>0){
-            return 2.0;
+    private void timeAdvance() {
+        if (state.v > 0){
+            continuousTime = inputPair.continuousTime + 2.0;
+            System.out.println("Time advanced to: " + continuousTime);
         } else {
-            return Double.POSITIVE_INFINITY;
+            continuousTime = Double.POSITIVE_INFINITY;
+            System.out.println("Time advanced to: " + continuousTime);
         }
     }
 
     private void lambda(){
         //sout coffee for 100 value AND combination of q,n,d of change representing v%100
-        while (state.v>=100){
+        while (state.v>=coffeeCost){
             System.out.println("Coffee dispensed");
-            state.v-=100;
-            change();
+            state.v-=coffeeCost;
         }
+        change();
     }
 
     private void deltaInternal(){
-        change();
-        timeAdvance();
+        System.out.println("In deltaInternal");
+        lambda();
     }
 
     private void deltaExternal(InputPair input){
+        System.out.println("In deltaExternal");
         takeInput(input);
-        change();
         timeAdvance();
     }
 
     private void deltaConfluent(InputPair input){
-        //if next internal event is before next external event
-
-
-
+        System.out.println("In deltaConfluent");
+        deltaInternal();
+        deltaExternal(input);
     }
 
     private void takeInput(InputPair input) {
@@ -75,6 +110,7 @@ public class Machine {
             state.q += 1;
             state.v += 25;
         }
+        System.out.println("State is: "+ state);
     }
 
     private void change(){
@@ -103,5 +139,4 @@ public class Machine {
         state.v = 0;
         Arrays.fill(outCounts, 0);
     }
-
 }
--- src/main/java/State.java
+++ src/main/java/State.java
@@ -1,10 +1,14 @@
 public class State {
     int q,n,d,v;
 
-    public State(int q, int n, int d, int v) {
-        this.q = q;
-        this.n = n;
-        this.d = d;
-        this.v = v;
+    public State() {
+        this.q = 0;
+        this.n = 0;
+        this.d = 0;
+        this.v = 0;
+    }
+
+    public String toString() {
+        return "q: " + q + " n: " + n + " d: " + d + " v: " + v;
     }
 }
